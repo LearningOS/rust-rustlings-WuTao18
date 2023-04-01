0bcfa6e8845d409d28f448a19accdc09106e20c3
WuTao18 <taowuuwoat@outlook.com>
2023-04-02 02:14:28 +0800

    feat: complete the remaining 47 exercises
    - options
    - error_handling
    - generics
    - traits
    - quiz3
    - tests
    - lifetimes
    - standard_library_types
    - threads
    - macros
    - clippy
    - conversions

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -23,7 +23,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
 
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
@@ -38,6 +37,23 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let r = match u8::try_from(tuple.0) {
+            Ok(r) => r,
+            Err(e) => return Err(IntoColorError::IntConversion),
+        };
+        let g = match u8::try_from(tuple.1) {
+            Ok(r) => r,
+            Err(e) => return Err(IntoColorError::IntConversion),
+        };
+        let b = match u8::try_from(tuple.2) {
+            Ok(r) => r,
+            Err(e) => return Err(IntoColorError::IntConversion),
+        };
+        Ok(Self {
+            red: r,
+            green: g,
+            blue: b,
+        })
     }
 }
 
@@ -45,6 +61,25 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        // let colors = arr
+        //     .iter()
+        //     .map(|&c| Ok(match u8::try_from(c) {
+        //         Ok(c) => c,
+        //         Err(e) => return Err(IntoColorError::IntConversion), // 这是闭包的返回值
+        //     }))
+        //     .collect::<Vec<u8>>();
+        let mut colors: Vec<u8> = vec![];
+        for c in arr {
+            colors.push(match u8::try_from(c) {
+                Ok(c) => c,
+                Err(e) => return Err(IntoColorError::IntConversion),
+            });
+        }
+        Ok(Self {
+            red: colors[0],
+            green: colors[1],
+            blue: colors[2],
+        })
     }
 }
 
@@ -52,6 +87,29 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        // let colors = slice
+        //     .iter()
+        //     .copied()
+        //     .map(|c| match u8::try_from(c) {
+        //         Ok(c) => c,
+        //         Err(e) => return Err(IntoColorError::IntConversion),
+        //     })
+        //     .collect();
+        let mut colors: Vec<u8> = vec![];
+        for c in slice {
+            colors.push(match u8::try_from(c.clone()) {
+                Ok(c) => c,
+                Err(e) => return Err(IntoColorError::IntConversion),
+            });
+        }
+        Ok(Self {
+            red: colors[0],
+            green: colors[1],
+            blue: colors[2],
+        })
     }
 }
 
